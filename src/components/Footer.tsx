82414cc2d9192a815d8306bd938f2a870f4e5e63
Thiago Moro <thiago.g.moro@gmail.com>
2025-09-06 21:47:56 +0100

    Adjusting the style.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-10">
+    <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center text-sm">
         Developed by <span className="font-semibold">Thiago Moro</span> © {new Date().getFullYear()}
       </div>
